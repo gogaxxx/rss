975af911b58696831817fdb4f8fe7a594037afe1
George Potapov <nephrite@inbox.ru>
2009-05-07 14:50:43 +0400

    bugfixes and beutification

--- cfg.pm
+++ cfg.pm
@@ -9,11 +9,13 @@ $mail_root      = '/home/nephrite/Mail/rss';
 $active_file    = '/home/nephrite/work/rss2mail-home/active';
 $default_period = 60;
 
-$root = '/home/nephrite/work/rss2mail-home';
+$root = '/home/nephrite/rss';
 
 $feeds_dir = "$root/feeds";
 $items_dir = "$root/items";
+$out_dir   = "$root/read";
 $guiddatabase = "$root/guids";
-$guidmaster   = "$root/guids.txt";
+$maxitems = 20;
+$date_format = '%a, %Y-%m-%d %H:%M:%S';
 
 1;

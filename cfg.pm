dc77667331002b5c6d8c8f4c51b0a880bb4ebf2a
GogaXXX <nephrite@inbox.ru>
2009-06-21 12:37:32 +0400

    added configurable sorting of news news can be ordered from older to newer or newer to older

--- cfg.pm
+++ cfg.pm
@@ -11,20 +11,71 @@ $default_period = 60;
 
 $root = '/home/nephrite/rss';
 
+#
+# feeds_dir - raw feeds from internet put in this directory
+#
 $feeds_dir = "$root/feeds";
+
+#
+# items_dir - processed items (one article per file) put in this
+#             directory
+#
 $items_dir = "$root/items";
+
+#
+# out_dir - compiled html-files, ready for reading (no pun intended) go
+#           in this directory
+#
 $out_dir   = "$root/read";
+
+#
+# guiddatabase - in this NDBM file seen guids are stored
+#
 $guiddatabase = "$root/guids";
+
+#
+# maxitems - maximum articles per html page
+#
 $maxitems = 20;
+
+#
+# date_format - in this format dates are printed near articles' headers.
+#               See strftime(3) for details
+#
 $date_format = '%a, %Y-%m-%d %H:%M:%S';
 
+#
+# Sorter can make ("compile") several different htmls from the same news
+# items. Here is how it is configured.
+# %to_process consists of several items in format name => config
+# <name> then supplied to sorter. config follows:
+#
+# input - index file with articles' data. parser produces two index
+#         files: "order" - all the articles ever seen, and "new" -
+#         articles seen the most recent run
+#
+# output_tmp - templates for compiled html files' names in sprintf
+#              format. One numeric parameter allowed: serial number of file
+#
+# sorting - articles' sorting method. Two possible values 'old_first' -
+#           older articles go first, the natural chronologic order.
+#           new_first - newer articles go first, reverse chronologic
+#           order, often used in blogs. This is the default.
+#
+# output_start - first file the user will point her browser at. It is a
+#                copy of one of output_tmp files and depend on sorting
+#                parameter. If sorting is 'old_first' then output_start
+#                will point to the first (the oldest) file, else it will
+#                point to the last (the newest) file
+#
 %to_process = (
 	lenta => { 'input'      => $cfg::items_dir.'/order',
 	'output_tmp'   => $cfg::out_dir.'/lenta%d.html',
 	'output_start' => $cfg::out_dir.'/lenta.html'},
 	news => { 'input'      => $cfg::items_dir.'/new',
 	'output_tmp'   => $cfg::out_dir.'/new%d.html',
-	'output_start' => $cfg::out_dir.'/new.html' },
+	'output_start' => $cfg::out_dir.'/new.html',
+    'sorting' => 'old_first'},
 );
 
 1;

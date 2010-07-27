0acdf257e66631834d268e97357a5cb6e98d566c
George Potapov <nephrite@inbox.ru>
2010-07-27 20:16:10 +0400

    поменял алгоритм определения типа фида должно ускориться

--- XML/Detector.pm
+++ XML/Detector.pm
@@ -1,57 +1,47 @@
 #
-# $Id: Detector.pm 29 2009-02-20 20:35:05Z nephrite $
+# $Id$
 #
 
 package XML::Detector;
 
 use strict;
-use fields qw(type);
-
-use XML::Parser::Expat;
-use base qw(XML::Parser::Expat);
+use warnings;
 
 sub new {
 	my $class=shift;
 
-	my $self=$class->SUPER::new(@_);
-	$self->reset();
-
+	my $self=bless {}, $class;
 	return $self;
 }
 
 sub detect {
 	my $self=shift;
-	my $content = shift;
+	my $content=shift;
 
-	$self->reset();
-	$self->parse($content);
+	my $b = 0;
 
-	return $self->{type};
-}
-
-sub reset {
-	my $self=shift;
-	undef $self->{type};
-	$self->setHandlers(Start => \&handle_start);
-}
-
-sub handle_start {
-	my ($exp, $el)=@_;
-
-	$el = lc($el);
-	if ($el eq 'rss'
-        || $el eq 'rdf:rdf') 
-    {
-		$exp->{type} = 'rss';
-		$exp->finish;
+	# найти первый тег
+	while ($b >= 0 && substr($content, $b+1, 1) !~ /[a-zA-Z]/o) {
+		$b = index($content, '<', $b+1);
 	}
-	elsif ($el eq 'feed') {
-		$exp->{type} = 'atom';
-		$exp->finish;
+
+	if ($b < 0) {
+		return 'error';
 	}
-	else {
-		$exp->{type} = 'error';
-		$exp->finish();
+
+	if (lc(substr($content, $b, length('<rss'))) eq '<rss'
+		||
+		lc(substr($content, $b, length('<rdf:rdf'))) eq '<rdf:rdf')
+	{
+		return 'rss';
+	}
+	elsif (lc(substr($content, $b, length('<feed'))) eq '<feed') 
+	{
+		return 'atom';	
+	}
+	else 
+	{
+		return 'error';
 	}
 }
 

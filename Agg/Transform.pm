9d1221d542755e664700ae19bb80b89ebfd299d8
GogaXXX <gogaxxx@gmail.com>
2012-09-30 21:43:53 +0400

    сделал возможное использование множества трансформаций итема путём последовательного применения цепочки трансформаторов

--- Agg/Transform.pm
+++ Agg/Transform.pm
@@ -3,32 +3,38 @@ package Agg::Transform;
 use strict;
 use warnings;
 
-# plugins
-use Agg::Transform::Idiot;
+# new #+++1
+sub new {
+	my $class=shift;
+	my ($cfg)=@_;
+
+	my $self = bless { cfg => $cfg}, $class;
+
+	my $chain = $cfg->{transform};
+	my @classes_chain = split(/\s*,\s*/o, $chain);
+
+	my @transformers_chain = ();
+	for my $tclass (@classes_chain) {
+		my $full_class='Agg::Transform::'.$tclass;
+		eval "require $full_class";
+
+		my $tr = $full_class->new($cfg);
+		push @transformers_chain, $tr;
+	}
+	$self->{chain} = \@transformers_chain;
+
+	return $self;
+}
 
 # transform_item #+++1
 sub transform_item {
+	my $self=shift;
 	my $item = shift;
-	# для всех: сцылки на youtube прописываем явно в тексте
-	my @videos = ();
-	while ($item->{body} =~ m{<[^>]+\=['"]?(http://(?:[^\.]+\.)?youtube\.com/[^'"\s>]+)}sg) { 
-		push @videos, $1;
-	}
-	$item->{body} .= '<br>'.join('<br>', map { 'Video: '.$_} @videos);
 
-	if ($item->{link} =~ /theregister\.com/o) {
-		# делать линк на печатную версию вместо обычной
-		$item->{link} .= '/print.html';
+	for my $tr (@{$self->{chain}}) {
+		$item = $tr->transform_item($item);
 	}
-	elsif ($item->{link} =~ /lj\.rossia\.org/o) {
-		# ljro shit # XXX dirty hack
-		$item->{body} =~ s/<\/?font[^>]*>//g;
-	}
-	elsif ($item->{link} =~ /idiottoys\.com/o) {
-		Agg::Transform::Idiot::transform($item);
-	}
-} 
-
-#---1
+	return $item;
+}
 
 1;

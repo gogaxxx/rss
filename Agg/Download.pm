07f98526a11fedb7b0f1f23165339f4bb3e5d141
GogaXXX <gogaxxx@gmail.com>
2012-10-05 03:18:38 +0400

    some useful methods 	modified:   Agg/Download.pm

--- Agg/Download.pm
+++ Agg/Download.pm
@@ -4,6 +4,8 @@
 
 package Agg::Download;
 
+use Digest::MD5 qw(md5_hex);
+use File::Path qw(make_path);
 use LWP::UserAgent;
 use constant DEBUG=>0;
 
@@ -40,6 +42,7 @@ sub new {
 		cmd_tpl => \@cmd,
 		filepos => \@filepos,
 		urlpos	=> \@urlpos,
+		root	=> $cfg->{'cache-root'},
 		cfg => $cfg}, $class;
 
 	return $self;
@@ -50,6 +53,8 @@ sub mirror {
 	my $self=shift;
 	my ($url, $filename, $return_content)=@_;
 
+	$url ||= $self->{'url'};
+	$filename ||= $self->{'full_path'};
 	$return_content //= 1;
 
 	warn "getting [$url]" if (DEBUG);
@@ -61,13 +66,94 @@ sub mirror {
 #		}
 
 	if ($return_content) {
-		my $content = '';
-		open(FILE, '<'.$filename) ||
-			die("[Agg::Download::mirror] Can't open $filename: $!");
-		$content = join('', <FILE>);
-		close FILE;
-		return $content;
+		return _slurp($filename);
 	}
+}
+
+# prepare #+++1
+sub prepare {
+	my $self=shift;
+	my ($url)=@_;
+
+	unless ($url =~ m{^http(s?)://}) {
+		$url = 'http://'.$self->{'transformer'}{'host'}.$url;
+	}
+	$self->{'url'} = $url;
+
+    my $hash = md5_hex(Encode::encode($self->{'cfg'}{'encoding'}, $url)); # дурь
+	$self->{'hash'} = $hash;
+	my $dir1 = substr($hash, 0, 2);
+	my $dir2 = substr($hash, 2, 2);
+	my $filename = substr($hash, 4);
+	my $dir = $self->{'root'}.'/'.$dir1.'/'.$dir2;
+	$self->{'dir'} = $dir;
+
+	unless (-d $dir) {
+		make_path($dir);
+	}
+
+    my $full_path = $dir.'/'.$filename;
+	$self->{'full_path'} = $full_path;
+}
+
+# load #+++1 
+#
+# всегда грузит локальную копию если она есть
+# если нет, скачивает с урла
+#
+sub load {
+	my $self=shift;
+	my $url = shift;
+
+	$self->prepare($url);
+	my $filename = $self->{'full_path'};
+	if (!-e $filename) {
+		$self->_mirror_download($url, $filename);
+	}
+
+	return _slurp($filename);
+}
+
+# fetch #+++1
+#
+# загрузить актуальную версию с урла, всегда проверяет на изменение
+#
+sub fetch {
+	my $self=shift;
+	my $url = shift;
+
+	$self->prepare($url);
+	my $filename = $self->{'full_path'};
+	$self->_mirror_download($url, $filename);
+	return _slurp($filename);
+}
+
+# cache #+++1 
+#
+# загрузить актуальную версию с урла, всегда проверяет на изменение, но
+# содержимое не возвращает
+#
+sub cache {
+	my $self=shift;
+	my $url=shift;
+
+	$self->prepare($url);
+	my $filename = $self->{'full_path'};
+	$self->_mirror_download($url, $filename);
+	return $filename;
+}
+
+# _slurp #+++1
+sub _slurp {
+	my $filename = shift;
+
+	my $content = '';
+	open(FILE, '<'.$filename) ||
+		die("[Agg::Download::mirror] Can't open $filename: $!");
+	$content = join('', <FILE>);
+	close FILE;
+
+	return $content;
 }
 
 ### _mirror_download ####### #+++1
